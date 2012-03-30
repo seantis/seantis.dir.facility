4238fd74382045b797084514190a5a5d4450fc94
Denis Krienbühl <denis.krienbuehl@gmail.com>
2012-03-30 17:18:04 +0200

    Added quick and dirty utils for directory.. will be reworked after the demo

--- seantis/dir/facility/directory.py
+++ seantis/dir/facility/directory.py
@@ -4,6 +4,7 @@ from zope.interface import implements
 from plone.namedfile.field import NamedImage
 from plone.autoform.interfaces import IFormFieldProvider
 from plone.directives import form
+from Products.CMFCore.utils import getToolByName
 
 from seantis.dir.base import directory
 from seantis.dir.base import core
@@ -11,6 +12,7 @@ from seantis.dir.base import core
 from seantis.dir.facility import _
 
 from seantis.reservation.overview import IOverview
+from seantis.reservation import utils
 
 class IFacilityDirectory(form.Schema):
     """Extends the seantis.dir.base.directory.IDirectory"""
@@ -27,10 +29,31 @@ class FacilityDirectoryFactory(core.DirectoryMetadataBase):
     interface = IFacilityDirectory
 
 class FacilityDirectory(directory.Directory):
-    pass
+    
+    def resources(self):
+        catalog = getToolByName(self, 'portal_catalog')
+        path = '/'.join(self.getPhysicalPath())
+
+        results = catalog(
+            path={'query': path, 'depth': 2},
+            portal_type='seantis.reservation.resource'
+        )
+
+        return [r.getObject() for r in results]
+
 
 class View(directory.View):
     implements(IOverview)
     grok.context(directory.IDirectory)
     grok.require('zope2.View')
     template = grok.PageTemplateFile('templates/directory.pt')
+
+    @property
+    def compare_link(self):
+        resources = self.context.resources()
+        return utils.compare_link(resources)
+
+    @property
+    def monthly_report_link(self):
+        resources = self.context.resources()
+        return utils.monthly_report_link(resources)
