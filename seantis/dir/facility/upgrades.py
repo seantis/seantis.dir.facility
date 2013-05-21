bbe0f4393105870af5ff4afdec73b78e7b62e86a
Denis Krienbühl <denis.krienbuehl@gmail.com>
2013-05-21 12:10:35 +0200

    Move image reset upgrade method to seantis.dir.base

--- seantis/dir/facility/upgrades.py
+++ seantis/dir/facility/upgrades.py
@@ -2,14 +2,10 @@ import logging
 log = logging.getLogger('seantis.dir.facility')
 
 from Products.CMFCore.utils import getToolByName
-from zope.component.hooks import getSite
 
-from StringIO import StringIO
-from plone.namedfile.file import NamedImage
-
-from seantis.reservation import utils as reservation_utils
+from seantis.dir.facility.directory import IFacilityDirectory
 from seantis.dir.facility.item import IFacilityDirectoryItem
-from seantis.dir.base.upgrades import add_behavior_to_item
+from seantis.dir.base.upgrades import add_behavior_to_item, reset_images
 
 profilemap = {
     'iZug Base Theme': 'izug_basetheme',
@@ -53,28 +49,8 @@ def upgrade_to_1000(context):
 
 
 def upgrade_1000_to_1001(context):
-    """ Plone 4.3 uses plone.namedfile 2.0.1 which fails on all existing images
-    of seantis.dir.facility. This function reapplies those images after which
-    everything is fine again. This unfortunately makes Plone 4.2 incompatible.
 
-    """
-
-    site = getSite()
-
-    brains = reservation_utils.portal_type_in_context(
-        site, 'seantis.dir.facility.item', depth=100
-    )
-    brains += reservation_utils.portal_type_in_context(
-        site, 'seantis.dir.facility.directory', depth=100
-    )
-
-    objects = [i.getObject() for i in brains]
-
-    for obj in objects:
-        if obj.image is None:
-            continue
-
-        obj.image = NamedImage(StringIO(obj.image.data), obj.image.contentType)
+    reset_images(context, (IFacilityDirectory, IFacilityDirectoryItem))
 
 
 def upgrade_1001_to_1002(context):
